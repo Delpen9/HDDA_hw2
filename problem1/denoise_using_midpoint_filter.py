2ce37d74b9eb83b1a46cbf8546da339b4d22998f
ian.dover <ian.dover@skunova.com>
2023-01-26 19:48:28 +0000

    made midpoint filter for denoising

---
+++ problem1/denoise_using_midpoint_filter.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+def midpoint_denoise(
+    image : np.ndarray,
+    _m : int,
+    _n : int
+):
+    '''
+    Applies a midpoint filter to denoise a colored image using the OpenCV library.
+    
+    Parameters:
+        image (ndarray): the image to be denoised
+        _m (int): the kernel width
+        _n (int): the kernel height
+    
+    Returns:
+        ndarray: the denoised image
+    '''
+    kernel = np.ones((_m, _n)).astype(int)
+    denoised_image = cv2.filter2D(image, -1, kernel)
+    return denoised_image
