3043fece47890d674529de2a4a619a167d91982a
ian.dover <ian.dover@skunova.com>
2023-02-04 20:10:21 +0000

    started problem 2 report

--- reports/problem2/problem_2_report_generator.py
+++ reports/problem2/problem_2_report_generator.py
@@ -1,68 +1,115 @@
 import numpy as np
-
-from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, \
-    Plot, Figure, Matrix, Alignat
-from pylatex.utils import italic
 import os
+from datetime import date
+
+from pylatex import *
+from pylatex.utils import *
+
+def generate_header(
+    doc : any,
+    todays_date : str,
+    company : str = 'Georgia Institute of Technology'
+) -> any:
+    '''
+    '''
+    header = PageStyle('header')
+
+    with header.create(Head('L')):
+        header.append('Page date: ')
+        header.append(LineBreak())
+        header.append(todays_date)
+    
+    with header.create(Head('C')):
+        header.append(company)
+
+    with header.create(Head('R')):
+        header.append(simple_page_number())
+
+    doc.preamble.append(header)
+    doc.change_document_style('header')
+
+    with doc.create(MiniPage(align = 'c')):
+        doc.append(LargeText(bold('HW2: Problem 2')))
+        doc.append(LineBreak())
+        doc.append(MediumText(bold('Ian Dover')))
+
+    return doc
 
 if __name__ == '__main__':
-    image_filename = os.path.join(os.path.dirname(__file__), 'kitten.jpg')
+    geometry_options = {'margin': '0.7in'}
+    doc = Document(geometry_options = geometry_options)
 
-    geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
-    doc = Document(geometry_options=geometry_options)
+    doc = generate_header(doc, date.today())
 
-    with doc.create(Section('The simple stuff')):
-        doc.append('Some regular text and some')
-        doc.append(italic('italic text. '))
-        doc.append('\nAlso some crazy characters: $&#{}')
-        with doc.create(Subsection('Math that is incorrect')):
-            doc.append(Math(data=['2*3', '=', 9]))
+    file_path = os.path.abspath(__file__)
+    parent_directory = os.path.dirname(file_path)
 
-        with doc.create(Subsection('Table of something')):
-            with doc.create(Tabular('rc|cl')) as table:
-                table.add_hline()
-                table.add_row((1, 2, 3, 4))
-                table.add_hline(1, 2)
-                table.add_empty_row()
-                table.add_row((4, 5, 6, 7))
+    # Part A
+    with doc.create(Section('Convert Image to Greyscale')):
+        doc.append('Image converted to greyscale:')
 
-    a = np.array([[100, 10, 20]]).T
-    M = np.matrix([[2, 3, 4],
-                   [0, 0, 1],
-                   [0, 0, 2]])
+        image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_Eagle_grayscale.jpg')
 
-    with doc.create(Section('The fancy stuff')):
-        with doc.create(Subsection('Correct matrix equations')):
-            doc.append(Math(data=[Matrix(M), Matrix(a), '=', Matrix(M * a)]))
+        with doc.create(Figure(position = 'h!')) as first_figure:
+            first_figure.add_image(image_path, width = '120px')
+            first_figure.add_caption('Greyscale image.')
 
-        with doc.create(Subsection('Alignat math environment')):
-            with doc.create(Alignat(numbering=False, escape=False)) as agn:
-                agn.append(r'\frac{a}{b} &= 0 \\')
-                agn.extend([Matrix(M), Matrix(a), '&=', Matrix(M * a)])
+    # Part B
+    with doc.create(Section('Part 1')):
+        with doc.create(Subsection('Sobel Threshold: 10')):
+            doc.append('Use the sobel operator on the image with a threshold of 10:')
 
-        with doc.create(Subsection('Beautiful graphs')):
-            with doc.create(TikZ()):
-                plot_options = 'height=4cm, width=6cm, grid=major'
-                with doc.create(Axis(options=plot_options)) as plot:
-                    plot.append(Plot(name='model', func='-x^5 - 242'))
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_10.jpg')
 
-                    coordinates = [
-                        (-4.77778, 2027.60977),
-                        (-3.55556, 347.84069),
-                        (-2.33333, 22.58953),
-                        (-1.11111, -493.50066),
-                        (0.11111, 46.66082),
-                        (1.33333, -205.56286),
-                        (2.55556, -341.40638),
-                        (3.77778, -1169.24780),
-                        (5.00000, -3269.56775),
-                    ]
+            with doc.create(Figure(position = 'h!')) as second_figure:
+                second_figure.add_image(image_path, width = '120px')
+                second_figure.add_caption('Sobel threshold of 10.')
 
-                    plot.append(Plot(name='estimate', coordinates=coordinates))
+        with doc.create(Subsection('Sobel Threshold: 50')):
+            doc.append('Use the sobel operator on the image with a threshold of 50:')
 
-        with doc.create(Subsection('Cute kitten pictures')):
-            with doc.create(Figure(position='h!')) as kitten_pic:
-                kitten_pic.add_image(image_filename, width='120px')
-                kitten_pic.add_caption('Look it\'s on its back')
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_50.jpg')
 
-    doc.generate_pdf('full', clean_tex=False)
+            with doc.create(Figure(position = 'h!')) as third_figure:
+                third_figure.add_image(image_path, width = '120px')
+                third_figure.add_caption('Sobel threshold of 50.')
+
+        with doc.create(Subsection('Sobel Threshold: 100')):
+            doc.append('Use the sobel operator on the image with a threshold of 100:')
+
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_100.jpg')
+
+            with doc.create(Figure(position = 'h!')) as fourth_figure:
+                fourth_figure.add_image(image_path, width = '120px')
+                fourth_figure.add_caption('Sobel threshold of 100.')
+
+        with doc.create(Subsection('Sobel Threshold: 150')):
+            doc.append('Use the sobel operator on the image with a threshold of 150:')
+
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_150.jpg')
+
+            with doc.create(Figure(position = 'h!')) as fifth_figure:
+                fifth_figure.add_image(image_path, width = '120px')
+                fifth_figure.add_caption('Sobel threshold of 150.')
+
+        with doc.create(Subsection('Sobel Threshold: 200')):
+            doc.append('Use the sobel operator on the image with a threshold of 200:')
+
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_200.jpg')
+
+            with doc.create(Figure(position = 'h!')) as sixth_figure:
+                sixth_figure.add_image(image_path, width = '120px')
+                sixth_figure.add_caption('Sobel threshold of 200.')
+
+        with doc.create(Subsection('Sobel Threshold: 255')):
+            doc.append('Use the sobel operator on the image with a threshold of 255:')
+
+            image_path = os.path.join(parent_directory, '..', '..', 'output', 'problem2', 'p2_part_1_sobel_thresh_255.jpg')
+
+            with doc.create(Figure(position = 'h!')) as seventh_figure:
+                seventh_figure.add_image(image_path, width = '120px')
+                seventh_figure.add_caption('Sobel threshold of 255.')
+
+    # doc.append(NewPage())
+
+    doc.generate_pdf('problem2', clean_tex = False)
